fe57a5f64e1d93deb19f4d2c6be1e68591a42efe
achingbrain <alex@achingbrain.net>
2014-11-06 14:44:26 +0000

    turned on https and auth by default

--- index.js
+++ index.js
@@ -1,4 +1,71 @@
+var commander = require('commander'),
+  pkg = require('./package.json'),
+  path = require('path'),
+  prompt = require('prompt'),
+  bcrypt = require('bcrypt')
 
-var Server = require('./server')
+colors = require('colors')
 
-new Server()
+var CLI = function() {
+
+}
+
+CLI.prototype.generateSalt = function() {
+  var salt = bcrypt.genSaltSync()
+  console.error('Please update the bossweb config file with:')
+  console.error('')
+  console.error('salt = %s'.cyan, salt)
+}
+
+CLI.prototype.generateUserPassword = function(userName) {
+  var config = require('./server/config')
+
+  prompt.message = ''
+  prompt.start()
+  prompt.get([{
+    name: 'Enter a password (no characters will appear)',
+    required: true,
+    hidden: true
+  }, {
+    name: 'Repeat the password',
+    required: true,
+    hidden: true,
+    message: 'Passwords must match',
+    conform: function (value) {
+      return value == prompt.history('Enter a password (no characters will appear)').value;
+    }
+  }], function (err, result) {
+    var password = result['Enter a password (no characters will appear)'];
+
+    console.info('')
+    console.info('Update your bossweb-users file with the following:')
+    console.info('')
+    console.info('[%s]'.cyan, userName)
+    console.info('  password = %s'.cyan, bcrypt.hashSync(password, config.salt))
+    console.info('')
+  }.bind(this))
+}
+
+var cli = new CLI()
+
+commander
+  .version(pkg.version)
+
+commander
+  .command('gensalt')
+  .description('Prints out config to add to bossweb to salt password hashes')
+  .action(cli.generateSalt.bind(cli))
+
+commander
+  .command('passwd <username>')
+  .description('Prints out config to add to bossweb-users to allow a user to authenticate')
+  .action(cli.generateUserPassword.bind(cli))
+
+var program = commander.parse(process.argv)
+
+// No command, start the webserver
+if(program.args.length == 0) {
+  var Server = require('./server')
+
+  new Server()
+}
--- server/index.js
+++ server/index.js
@@ -5,185 +5,229 @@ var Container = require('wantsit').Container,
   EventEmitter = require('wildemitter'),
   util = require('util'),
   fs = require('fs'),
-  clientConfig = coerce(stripUnderscores(require('rc')('boss/bossweb-client', path.resolve(__dirname + '/../bossweb-client')))),
+  os = require('os'),
   logger = require('andlog'),
   Hapi = require('hapi'),
+  HapiSession = require('hapi-session'),
+  BasicAuth = require('hapi-auth-basic'),
   Columbo = require("columbo"),
-  hostConfig = coerce(checkLength(stripUnderscores(require('rc')('boss/bossweb-hosts')), 'Please define some host names')),
-  userConfig = coerce(checkLength(stripUnderscores(require('rc')('boss/bossweb-users')), 'Please define some users')),
-  config = coerce(stripUnderscores(require('rc')('boss/bossweb', path.resolve(__dirname + '/../bossweb'))))
-
-function coerce(obj) {
-  if(Array.isArray(obj)) {
-    return obj.map(function(value) {
-      return coerce(value)
-    })
-  }
-
-  if(obj instanceof String || typeof obj == 'string') {
-    if(obj == 'false') {
-      return false
-    }
-
-    if(obj == 'true') {
-      return true
-    }
-
-    if(isFinite(obj)) {
-      return parseFloat(obj)
-    }
-
-    return obj
-  }
-
-  if(obj === true || obj === false) {
-    return obj
-  }
-
-  for(var key in obj) {
-    obj[key] = coerce(obj[key])
-  }
-
-  return obj
-}
-
-function stripUnderscores(obj) {
-  for(var key in obj) {
-    if(key == '_') {
-      delete obj[key]
-    }
-  }
-
-  return obj
-}
-
-function checkLength(obj, message) {
-  if(Object.keys(obj).length == 0) {
-    console.error(message)
-    process.exit(1)
-  }
-
-  return obj
-}
-
-Object.keys(userConfig).forEach(function(userName) {
-  for(var key in userConfig[userName]) {
-    if(key == 'password') {
-      continue
-    }
-
-    if(!hostConfig[key]) {
-      console.error('User \'%s\' has config set for host \'%s\' but no such host exists in bossweb-hosts - please fix your configuration', userName, key)
-      process.exit(1)
-    }
-  }
-})
-
-config.hosts = hostConfig
-config.users = userConfig
+  config = require('./config'),
+  pem = require('pem'),
+  async = require('async'),
+  http = require('http'),
+  bcrypt = require('bcrypt')
 
 BossWeb = function() {
 
-  // create container
-  this._container = new Container();
+  // make errors a little more descriptive
+  process.on("uncaughtException", function (exception) {
+    container.find("logger").error("Uncaught exception", exception && exception.stack ? exception.stack : "No stack trace available")
 
-  // set up logging
-  this._container.register("logger", logger)
+    throw exception
+  }.bind(this))
+
+  // create container
+  var container = new Container();
 
   // parse configuration
-  this._container.register("config", config);
+  container.register("config", config)
+
+  // set up logging
+  container.register("logger", logger)
 
   // web sockets
-//  this._container.createAndRegister("webSocketResponder", require("./components/WebSocketResponder"));
-//  this._container.createAndRegister("webSocketServer", WebSocketServer, {
+//  container.createAndRegister("webSocketResponder", require("./components/WebSocketResponder"));
+//  container.createAndRegister("webSocketServer", WebSocketServer, {
 //    server: this._server,
 //    path: "/ws"
 //  });
 
   // object factories
-  this._container.createAndRegister("hostDataFactory", ObjectFactory, require("./domain/HostData"))
-  this._container.createAndRegister("processDataFactory", ObjectFactory, require("./domain/ProcessData"))
+  container.createAndRegister("hostDataFactory", ObjectFactory, require("./domain/HostData"))
+  container.createAndRegister("processDataFactory", ObjectFactory, require("./domain/ProcessData"))
 
   // holds host data
-  this._container.createAndRegister("hostList", require("./components/HostList"));
+  container.createAndRegister("hostList", require("./components/HostList"));
 
-  // make errors a little more descriptive
-  process.on("uncaughtException", function (exception) {
-    this._container.find("logger").error("Uncaught exception", exception && exception.stack ? exception.stack : "No stack trace available")
+  var tasks = []
 
-    throw exception
-  }.bind(this));
+  if(config.https.enabled) {
+    if(config.https.key && config.https.cert) {
+      tasks.push(function(callback) {
+        logger.info('Reading SSL key and certificate')
 
-  var internals = {
-    clientConfig: JSON.stringify(clientConfig.client),
-
-    // set clientconfig cookie
-    configStateConfig: {
-      encoding: 'none',
-      ttl: 1000 * 60 * 15,
-      isSecure: clientConfig.isSecure
+        async.parallel([
+          fs.readFile.bind(fs, config.https.key, {
+            encoding: 'utf8'
+          }),
+          fs.readFile.bind(fs, config.https.cert, {
+            encoding: 'utf8'
+          })
+        ], function(error, result) {
+          callback(error, {
+            serviceKey: result[0],
+            certificate: result[1],
+            passphrase: config.https.passphrase
+          })
+        })
+      })
+    } else {
+      tasks.push(function(callback) {
+        logger.info('Generating SSL key and certificate')
+        pem.createCertificate({days:1, selfSigned:true}, callback)
+      })
     }
+  } else {
+    logger.info('HTTPS is disabled')
   }
 
-  var server = Hapi.createServer(config.http.listen, config.http.port)
+  async.series(tasks, function(error, result) {
+    if(error) throw error
 
-  server.state('config', internals.configStateConfig)
+    var internals = {
+      clientConfig: JSON.stringify(config.client.client),
 
-  server.ext('onPreResponse', function(request, reply) {
-    if (!request.state.config) {
-      return reply(request.response.state('config', encodeURIComponent(internals.clientConfig)))
+      // set clientconfig cookie
+      configStateConfig: {
+        encoding: 'none',
+        ttl: 1000 * 60 * 15,
+        isSecure: config.https.enabled
+      }
     }
-    else {
-      return reply()
+
+    var options = {}
+    var host = config.http.listen
+    var port = config.http.port
+
+    if(config.https.enabled) {
+      options.tls = {
+        key: result[0].serviceKey,
+        cert: result[0].certificate,
+        passphrase: result[0].passphrase,
+        ciphers: 'ECDHE-RSA-AES256-SHA:AES256-SHA:RC4-SHA:RC4:HIGH:!MD5:!aNULL:!EDH:!AESGCM',
+        honorCipherOrder: true
+      }
+
+      host = config.https.listen
+      port = config.https.port
     }
-  })
 
-  // require moonboots_hapi plugin
-  server.pack.register({plugin: require('moonboots_hapi'), options: require('./moonboots')}, function (err) {
-    if (err) throw err
+    if(config.https.upgrade) {
+      logger.info('Starting HTTP -> HTTPS upgrade server')
 
-    server.start(function (err) {
-      if (err) throw err
-      console.log("boss-web is running at: http://localhost:%d", config.http.port)
+      process.nextTick(function() {
+        http.createServer(function(request, response) {
+          var host = request.headers.host
+
+          if(!host) {
+            host = os.hostname()
+          } else {
+            host = host.split(':')[0]
+          }
+
+          // create an app that will redirect all requests to the https version
+          var httpsUrl = "https://" + host
+
+          if(config.https.port != 443) {
+            httpsUrl += ":" + config.https.port;
+          }
+
+          if(request.url) {
+            httpsUrl += request.url
+          }
+
+          response.setHeader('location', httpsUrl)
+          response.statusCode = 302
+          response.end()
+        }).listen(config.http.port, config.http.host, function() {
+          logger.info('HTTP -> HTTPS upgrade server listening on port', config.http.port);
+        })
+      })
+    }
+
+    var hapi = Hapi.createServer(host, port, options)
+
+    hapi.state('config', internals.configStateConfig)
+
+    hapi.ext('onPreResponse', function(request, reply) {
+      if (!request.state.config) {
+        return reply(request.response.state('config', encodeURIComponent(internals.clientConfig)))
+      }
+      else {
+        return reply()
+      }
     })
-  })
+    /*hapi.auth('session', {
+      implementation: new HapiSession(hapi, {
 
-  server.route({
-    method: 'GET',
-    path: '/images/{param*}',
-    handler: {
-      directory: {
-        path: path.resolve(__dirname + '/../public/images')
+      })
+    })*/
+    hapi.pack.register(BasicAuth, function (error) {
+      hapi.auth.strategy('simple', 'basic', true, {
+        validateFunc: function (userName, password, callback) {
+          var user = config.users[userName]
+
+          if (!user) {
+            return callback(null, false)
+          }
+
+          bcrypt.compare(password, user.password, function (error, isValid) {
+            callback(error, isValid, {
+              user: userName
+            })
+          })
+        }
+      })
+    })
+    hapi.pack.register({plugin: require('moonboots_hapi'), options: require('./moonboots')}, function (err) {
+      if (err) throw err
+
+      hapi.start(function (err) {
+        if (err) throw err
+        console.log("boss-web is running at: http://localhost:%d", hapi.info.port)
+      })
+    })
+
+    hapi.route({
+      method: 'GET',
+      path: '/images/{param*}',
+      handler: {
+        directory: {
+          path: path.resolve(__dirname + '/../public/images')
+        }
       }
-    }
-  })
-  server.route({
-    method: 'GET',
-    path: '/fonts/{param*}',
-    handler: {
-      directory: {
-        path: path.resolve(__dirname + '/../public/fonts')
+    })
+    hapi.route({
+      method: 'GET',
+      path: '/fonts/{param*}',
+      handler: {
+        directory: {
+          path: path.resolve(__dirname + '/../public/fonts')
+        }
       }
-    }
-  })
-  server.route({
-    method: 'GET',
-    path: '/apple-touch-icon.png',
-    handler: function (request, reply) {
-      reply.file(path.resolve(__dirname + '/../public/apple-touch-icon.png'))
-    }
-  })
+    })
+    hapi.route({
+      method: 'GET',
+      path: '/apple-touch-icon.png',
+      config: {
+        auth: false
+      },
+      handler: function (request, reply) {
+        reply.file(path.resolve(__dirname + '/../public/apple-touch-icon.png'))
+      }
+    })
 
-  var columbo = new Columbo({
-    resourceDirectory: path.resolve(__dirname + '/resources'),
-    resourceCreator: function(resource, name) {
-      return this._container.createAndRegister(name, resource)
-    }.bind(this),
-    logger: logger
-  })
+    var columbo = new Columbo({
+      resourceDirectory: path.resolve(__dirname + '/resources'),
+      resourceCreator: function(resource, name) {
+        return container.createAndRegister(name, resource)
+      },
+      logger: logger
+    })
 
-  server.route(columbo.discover())
-  server.start()
+    hapi.route(columbo.discover())
+    hapi.start()
+  })
 }
 
-module.exports = BossWeb;
+module.exports = BossWeb
